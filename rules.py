bc4b15d27b14a09d65b9c4919a8c5831e94578ea
Ananya V <ananyav2076@gmail.com>
2026-07-24 10:41:28 +0530

    Update rules.py
    added another parameter into the functions in rules.py file

--- rules.py
+++ rules.py
@@ -12,22 +12,24 @@ def detect_brute_force():
     # gives the exact timestamp of "60 seconds ago" - our cutoff for "recent" events
 
     # counting the number of LOGIN_FAILED events by each ip in the last 60 secs
-    cur.execute("""SELECT source_ip, COUNT (*) FROM events 
+    cur.execute("""SELECT source_ip, COUNT (*), GROUP_CONCAT(event_id) FROM events 
                 WHERE event_type = 'LOGIN_FAILED' AND timestamp > ?
                 GROUP BY source_ip""", (cutoff,))  # '*' - adds all the rows
-    # each row is a tuple like (source_ip, count)
+    # each row is a tuple like (source_ip, count, related_event_ids); related_event_ids is a string
     rows = cur.fetchall()
     con.close()
 
     # if ip count exceeds threshold value (10) within last 60 secs, raise an alert
-    for (ip, count) in rows:
+    rule_name = "BRUTE_FORCE_DETECTED"
+    for (ip, count, related_event_ids) in rows:
         if count >= 10:
-            if not models.is_suppressed(ip, "BRUTE_FORCE_DETECTED"):
-                score, severity = calculate_score("BRUTE_FORCE_DETECTED", count, 10)
-                models.insert_alert(ip, "BRUTE_FORCE_DETECTED", score, severity)
-                print("Alert - more than 10 attempts in 60 secs")
+            if not models.is_suppressed(ip, rule_name):
+                score, severity = calculate_score(rule_name, count, 10)
+                models.insert_alert(ip, rule_name, score, severity, related_event_ids)
+                print(f"[{severity}] {rule_name} - {ip} made {count} failed login attempts in the last 60 seconds (score: {score})")
             else:
                 print(f"Suppressed ip -{ip} matched a known suppression rule")
+# related_event_ids would be used in SIEM such that you can click on a specific alert and see exactly which raw events caused it - not 
 
 # port scan detection rule 
 def detect_port_scan():
@@ -36,19 +38,21 @@ def detect_port_scan():
     cutoff = str(datetime.datetime.now() - datetime.timedelta(seconds=60))
 
     # COUNT(DISTINCT target) counts unique ports touched per IP, not total attempts
-    cur.execute("""SELECT source_ip, COUNT (DISTINCT target) FROM events
+    cur.execute("""SELECT source_ip, COUNT (DISTINCT target), GROUP_CONCAT(event_id) FROM events
                 WHERE event_type='PORT_SCAN_ATTEMPT' AND timestamp > ?
                 GROUP BY source_ip""", (cutoff, ))
+    # GROUP_CONCAT concatenates the events ids of the source ips that caused alert into a string
     rows = cur.fetchall()
     con.close()
 
     # alert raised if 15 or more distinct ports touched within last 60 secs
-    for (ip, count) in rows:
+    rule_name = "PORT_SCAN_DETECTED"
+    for (ip, count, related_event_ids) in rows:
         if count >= 15: 
-            if not models.is_suppressed(ip, "PORT_SCAN_DETECTED"):
-                score, severity = calculate_score("PORT_SCAN_DETECTED", count, 15)
-                models.insert_alert(ip, "PORT_SCAN_DETECTED", score, severity)
-                print("Alert - too many distinct ports")
+            if not models.is_suppressed(ip, rule_name):
+                score, severity = calculate_score(rule_name, count, 15)
+                models.insert_alert(ip, rule_name, score, severity, related_event_ids)
+                print(f"[{severity}] {rule_name} - {ip} touched {count} disinct ports in the last 60 seconds (score: {score})")
             else:
                 print(f"Suppressed ip -{ip} matched a known suppression rule")
 
@@ -74,3 +78,6 @@ def calculate_score(rule_name, count, threshold):
 
 
 # in readme, write about deltatime, group by and all that 
+
+
+# in readme - group_concat, stored but not used, will not be using it, but used in real siem 
